8e59858d81ceaa3bdbc11962a6756b95ffef2a62
Spagecko <bennettlawrenz@gmail.com>
2020-05-25 15:02:08 -0700

    Add files via upload

--- containers/Register.js
+++ containers/Register.js
@@ -1,15 +1,43 @@
-import React from 'react'; 
-import { Text, View, StyleSheet, FlatList } from 'react-native';
-RegisterPage = () =>(
-    <View>
-        <Text style = {styles.textStyle}>Hello Register Page</Text>
-    </View>
+import React from 'react';
+import { Text, View, StyleSheet, Button, ScrollView ,TextInput } from 'react-native';
+import {Input } from 'react-native-elements';
 
-)
+ export default class RegisterPage extends  React.Component
+{
+    state = { 
+        username: '',
+        password: '', 
+        confirmPassword:','
+    }
+    
+
+    render() { 
+         return (<ScrollView style = {{padding: 20 }}>
+
+             <Text style = {{fontsize : 27 }}>
+                 Login
+             </Text>
+             <Input inputStyle = {styles.inputStyle} placeholder = "Username"></Input>
+             <Input inputStyle = {styles.inputStyle} placeholder = "password"></Input>
+             <Input inputStyle = {styles.inputStyle} placeholder = " Confirm password"></Input>
+             <View style ={{margin: 7 }} /> 
+             <Button title = "Sign Up"></Button>
+        
+         </ScrollView>);
+    }
+}
 const styles = StyleSheet.create({
     textStyle: {
         color:'#faf8f2',
+    },
+
+    inputStyle :{
+        color: '#faf8f2',
+        backgroundColor: '#4A4C63  ',
+        padding: 20, 
+        
+        
+
     }
 })
 
-export default RegisterPage;
--- containers/loginPage.js
+++ containers/loginPage.js
@@ -1,18 +1,44 @@
-import React from 'react'; 
-import { Text, View, StyleSheet, FlatList } from 'react-native';
-const loginPage  =   ({ route, navigation })=>{
-    return(
-    <View>
-        <Text style = {styles.textStyle}>Hello Login Page</Text>
-    </View>
-    );
+import React from 'react';
+import { Text, View, StyleSheet, Button, ScrollView ,TextInput } from 'react-native';
+import {Input } from 'react-native-elements';
 
+ export default class LoginPage extends  React.Component
+{
+    state = { 
+        username: '',
+        password: '', 
+       /// isLogin: false, 
+    }
+
+    render() { 
+        
+    console.log(this.isLoggedIn); 
+         return (<ScrollView style = {{padding: 20 }}>
+
+             <Text style = {{fontsize : 27 }}>
+                 Login
+             </Text>
+             <Input inputStyle = {styles.inputStyle} placeholder = "Username"></Input>
+             <Input inputStyle = {styles.inputStyle} placeholder = "password"></Input>
+             <View style ={{margin: 7 }} /> 
+             <Button title = "Submit"></Button>
+        
+         </ScrollView>);
+    }
 }
 const styles = StyleSheet.create({
     textStyle: {
         color:'#faf8f2',
+    },
+
+    inputStyle :{
+        color: '#faf8f2',
+        backgroundColor: '#4A4C63  ',
+        padding: 20, 
+        
+        
+
     }
 })
 
 
-export default loginPage;
